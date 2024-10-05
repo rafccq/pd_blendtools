7b92e7aa3b020be7dfda067510ae249468aac024
Raf <>
2024-10-05 21:36:40 +0800

    meshes: separate mesh from XLU layers; come clean up, refactored createMesh a bit (passing less params)

--- pd_modeltool/cooking.py
+++ pd_modeltool/cooking.py
@@ -4,6 +4,8 @@ import logging
 import bpy
 import bmesh
 from bpy import context as ctx
+from bpy.props import StringProperty, IntProperty, PointerProperty
+from bpy.types import PropertyGroup, Object, Panel
 from mathutils import Euler, Vector, Matrix
 
 import pd_utils as pdu
@@ -13,87 +15,105 @@ import nodes.pd_shadernodes as pdn
 from pd_materials import *
 from pdmodel import unmask
 from gbi import *
+import export as exp
 
-
-logging.basicConfig(filename='D:/Mega/PD/pd_blend/pd_guns.log',
-    level=logging.DEBUG,
-    format="{asctime}[{levelname}] {message}",
-    style="{",
-    datefmt="%Y-%m-%d %H:%M",
-    filemode='w+')
 logger = logging.getLogger(__name__)
 logger.handlers.clear()
-logger.propagate = False
 logger.setLevel(logging.DEBUG)
-# logger.addHandler(logging.StreamHandler())
-logger.addHandler(logging.FileHandler('D:/Mega/PD/pd_blend/pd_guns.log'))
-# logging.basicConfig(filename='D:/Mega/PD/pd_blend/pd_guns.log', encoding='utf-8', level=logging.DEBUG, filemode='w')
+
+fh = logging.FileHandler('D:/Mega/PD/pd_blend/pd_guns.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+# ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y%m%d %H:%M:%S')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 def createCollectionIfNone(name):
     if name not in bpy.data.collections:
         collection = bpy.data.collections.new(name)
         ctx.scene.collection.children.link(collection)
 
-def createMesh(verts, faces, idx, sub_idx, tri2tex, mtxindex, tex_configs):
+def createMesh(mesh, tex_configs, idx, sub_idx):
     mesh_data = bpy.data.meshes.new('mesh_data')
+
+    verts = mesh.verts
+    faces = mesh.tris
+    tri2tex = mesh.tri2tex
 
     verts_xyz = [v.pos for v in verts]
     mesh_data.from_pydata(verts_xyz, [], faces)
+    mesh_data.validate(verbose=True)
 
-    suffix = f'[{sub_idx}]-M{mtxindex:02X}' if sub_idx >= 0 else f'-M{mtxindex:02X}'
+    # suffix = f'[{sub_idx}]-M{mtxindex:02X}' if sub_idx >= 0 else f'-M{mtxindex:02X}'
+    suffix = f'[{sub_idx}]' if sub_idx >= 0 else ''
+    suffix += '.XLU' if mesh.layer == MeshLayer.XLU else ''
     name = f'{idx:02X}.Mesh{suffix}'
+
     obj = bpy.data.objects.new(name, mesh_data)
-    # obj.location = (pos[0], pos[2], pos[1])
-    # bpy.data.collections['Meshes'].objects.link(obj)
+    obj.pdmodel_props.name = name
+    obj.pdmodel_props.idx = idx
+    obj.pdmodel_props.sub_idx = sub_idx
+    obj.pdmodel_props.mtx = mesh.mtxindex
+
     collection = pdu.active_collection()
     collection.objects.link(obj)
 
-    layer_vtxcol = mesh_data.vertex_colors
-    logger.debug(f'[CREATEMESH {idx:02X}] {name} vtxcol {layer_vtxcol} len {len(layer_vtxcol)}')
+    logger.debug(f'[CREATEMESH {idx:02X}] {name}')
 
-    # setup vertex normals
-    hasnormals = len(verts) and verts[0].hasnormal # assuming is either all norm or all color
-    hascolors = not hasnormals
     normals = []
+    colors = []
 
-    colattr = None
-    if hascolors:
-        colattr = obj.data.color_attributes.new(
-            name='vtxcolor',
-            type='FLOAT_COLOR',
-            domain='POINT',
-        )
-
-    for idx, v in enumerate(verts):
+    for i, v in enumerate(verts):
         normcolor = v.color # either a normal or color
         if normcolor is None: continue
 
+        hascolors = not v.hasnormal
+
         if hascolors:
-            r = normcolor[0]/255.0
-            g = normcolor[1]/255.0
-            b = normcolor[2]/255.0
-            a = normcolor[3]/255.0
-            # logger.debug(f'  {r:.2f} {g:.2f} {b:.2f}')
-            colattr.data[idx].color = [r, g, b, a]
+            colors.append(tuple([ci/255.0 for ci in normcolor]))
+            normals.append((0,0,0))
         else:
-            s = 2.2
-            n = Vector((normcolor[0], normcolor[2], normcolor[1])).normalized()
-            n.x *= s; n.y *= s; n.z *= s
-            normals.append(n)
+            vn = tuple([int.from_bytes(normcolor[i:i+1], 'big', signed=True) for i in range(0,3)])
+            n = Vector((vn[0], vn[2], vn[1])) #invert-yz
+            # n = Vector((-1,0,0))
+            normals.append(n.normalized())
+            # normals.append(n)
+            # for k in range(0,3): c[k] *= 255
+            c = [float(ci/255) for ci in normcolor]
+            # colors.append((c[0],c[1],c[2],1))
+            colors.append((1,1,1,1))
 
-    me = obj.data
+        c = normcolor
+        txtnorm = '' if hascolors else ' (N)'
+        txtnorm += '' if hascolors else ' (' + ''.join([f'{ni:<6.3f} ' for ni in normals[i]]) + ')'
+        txtidx = f' [{v.color_idx}]'
+        if idx == 0x27: print(f'vc {i:03d} ({c[0]:02X} {c[1]:02X} {c[2]:02X} {c[3]:02X}){txtnorm}{txtidx}')
 
-    bpy.context.view_layer.objects.active = obj
+    mesh_data.normals_split_custom_set_from_vertices(normals)
+    mesh_data.update()
 
-    bpy.ops.object.mode_set(mode = 'EDIT')
-    bm = bmesh.from_edit_mesh(me)
+    me = mesh_data
+    obj['pdmodel_idx'] = idx
+    obj['pdmodel_mtx'] = mesh.mtxindex
+
+    bm = bmesh.new()
+    bm.from_mesh(me)
+
     bm.faces.ensure_lookup_table()
     bm.verts.ensure_lookup_table()
 
     # setup UVs and materials
     uv_layer = bm.loops.layers.uv.verify()
+    color_layer = bm.loops.layers.color.new("vtxcolor")
     for idx, face in enumerate(bm.faces):
         matsetup = tri2tex[face.index]
+        # logger.debug(f'face {idx} mat {matsetup.id()}')
         tc = tex_configs[matsetup.texnum]
 
         matname = matsetup.id()
@@ -106,24 +126,36 @@ def createMesh(verts, faces, idx, sub_idx, tri2tex, mtxindex, tex_configs):
 
         face.material_index = mat_idx
 
-        # setup the verts uv data
+        # setup the verts uv data and colors
         for loop in face.loops:
             vert = verts[loop.vert.index]
             uv_sc = (1 / (32 * tc['width']), 1 / (32 * tc['height']))
             uv = (vert.uv[0] * uv_sc[0], vert.uv[1] * uv_sc[1])
             loop[uv_layer].uv = uv
+            loop[color_layer] = colors[loop.vert.index]
 
-    bmesh.update_edit_mesh(me)
+    # remove unused vertices
+    verts_unused = []
+    for idx,v in enumerate(bm.verts):
+        if len(v.link_loops) == 0:
+            logger.debug(f'v {v.index} (NO GEOMETRY)')
+            verts_unused.append(v)
+
+    for v in verts_unused:
+        bm.verts.remove(v)
+
+    # bmesh.update_edit_mesh(me)
+    bm.to_mesh(me)
     bm.free()
 
-    bpy.ops.object.mode_set(mode = 'OBJECT')
+    # bpy.ops.object.mode_set(mode = 'OBJECT')
 
     return obj
 
-def readModel(modelName):
+def readModel(modelName, skipDLdata=False):
     print('-'*32)
     blend_dir = os.path.dirname(bpy.data.filepath)
-    model = pdmodel.read(f'{blend_dir}/files', modelName)
+    model = pdmodel.read(f'{blend_dir}/files', modelName, skipDLdata)
     idx = 0
     for addr, node in model.nodes.items():
         nodetype = node['type']
@@ -180,13 +212,19 @@ def create_joint(model, node, idx, depth, **kwargs):
         createObj(idx, (x, z, y), parentobj)
 
 def _t(v): return f'({v[0]:.2f}, {v[1]:.2f}, {v[2]:.2f})'
+
+from enum import Enum
+class MeshLayer(Enum):
+    OPA = 0
+    XLU = 1
+
 class SubMesh:
-    def __init__(self ):
+    def __init__(self, mtxindex, layer):
         self.nverts = 0
         self.vtx_start = -1
         self.vtx_end = -1
         self.vtx_offset = -1
-        self.mtxindex = 0
+        self.mtxindex = mtxindex
         self.verts = []
         self.tris = []
         self.tri_index = 0
@@ -194,6 +232,7 @@ class SubMesh:
         self.dbg = 0
         self.vtx_buffer = []
         self.tri2tex = {} # tri -> texnum
+        self.layer = layer
 
     def vtx_from_buffer(self, vtx_buffer, n):
         if len(self.vtx_buffer): return
@@ -225,10 +264,11 @@ class SubMesh:
         self.vtx_buffer.clear()
 
 class VtxBufferEntry:
-    def __init__(self, pos=(0,0,0), uv=(0,0), color=None, group_size=0, mtxidx=-1, hasnormal=False):
+    def __init__(self, pos=(0,0,0), uv=(0,0), color=None, color_idx=0, group_size=0, mtxidx=-1, hasnormal=False):
         self.pos = pos
         self.uv = uv
         self.color = color
+        self.color_idx = color_idx
         self.group_size = group_size
         self.mtxidx = mtxidx
         self.meshes_loaded = []
@@ -261,7 +301,7 @@ def collectSubMeshes(model, rodata, idx):
     addr = 0
     bo='big'
     dbg = idx == 0x3
-    # dbg = 1
+    dbg = 1
 
     matrixmesh_map = {} # matrix idx -> mesh
     mtxindex = -1
@@ -287,6 +327,8 @@ def collectSubMeshes(model, rodata, idx):
         G_TEXTURE, G_PDTEX
     ]
 
+    layer = MeshLayer.OPA
+
     while True:
         cmd = gdl[addr:addr+8]
         op = cmd[0]
@@ -297,6 +339,8 @@ def collectSubMeshes(model, rodata, idx):
                 gdl = model.data(ptr_xlugdl)
                 addr = 0
                 gdlnum += 1
+                layer = MeshLayer.XLU
+                matrixmesh_map[(mtxindex, layer)] = SubMesh(mtxindex, layer)
                 continue
             else:
                 break
@@ -316,7 +360,7 @@ def collectSubMeshes(model, rodata, idx):
                 v = select_mesh(tri, vtx_buffer)
                 if dbg: logger.debug(f'tri {tri} tex {mat_setup.texnum:04X}')
 
-                mesh = matrixmesh_map[v.mtxidx]
+                mesh = matrixmesh_map[(v.mtxidx, layer)]
                 mesh.vtx_from_buffer(vtx_buffer, vtx_buf_size)
                 mesh.add_tri(tri, mat_setup)
         elif op == G_VTX:
@@ -337,23 +381,21 @@ def collectSubMeshes(model, rodata, idx):
 
                 uv = (v[3], v[4])
                 coloridx = v[5]
-                color = col_data[coloridx:coloridx+4]
-                if dbg: logger.debug(f'  {i} {coloridx} mtx {mtxindex}')
-                vtx_buffer[vtx_idx+i] = VtxBufferEntry(vpos, uv, color, nverts, mtxindex)
+                c = color = col_data[coloridx:coloridx+4]
+                if dbg: logger.debug(f'  {i} col {coloridx} ({c[0]:02X} {c[1]:02X} {c[2]:02X} {c[3]:02X}) mtx {mtxindex}')
+                hasnormal = mat_setup.has_normal()
+                vtx_buffer[vtx_idx+i] = VtxBufferEntry(vpos, uv, color, coloridx, nverts, mtxindex, hasnormal)
 
-            matrixmesh_map[mtxindex].clear_vtx_buffer()
+            matrixmesh_map[(mtxindex, layer)].clear_vtx_buffer()
         elif op == G_COL:
             col_start = (cmdint & 0xffffff)
         elif op == G_MTX:
             mtxindex = (cmdint & 0xffffff) // 0x40
             logger.debug(f'  MTX {mtxindex:04X}')
 
-            if mtxindex not in matrixmesh_map:
-                currentSubMesh = matrixmesh_map[mtxindex] = SubMesh()
-                currentSubMesh.mtxindex = mtxindex
-                currentSubMesh.dbg = dbg
-        elif op == G_RDPPIPESYNC:
-            mat_setup = PDMaterialSetup()
+            if (mtxindex, layer) not in matrixmesh_map:
+                cur_mesh = matrixmesh_map[(mtxindex, layer)] = SubMesh(mtxindex, layer)
+                cur_mesh.dbg = dbg
         elif op in MAT_CMDS:
             if mat_setup.applied:
                 mat_setup = PDMaterialSetup(mat_setup)
@@ -364,18 +406,18 @@ def collectSubMeshes(model, rodata, idx):
     return list(filter(lambda e: len(e.tris), matrixmesh_map.values()))
 
 def createModelMesh(idx, model, rodata, sc, tex_configs, parent_obj):
+    # if idx in [0x36, 0x3a, 0x3c, 0x3e, 0x38, 0x40]: return
     # logger.debug(f'createModelMesh {idx:02X}')
 
     subMeshes = collectSubMeshes(model, rodata, idx)
-    # colors = rodata['colors']['bytes']
     n_submeshes = len(subMeshes)
     logger.debug(f'idx {idx:02X} n {n_submeshes}')
-    for sub_idx, mesh in enumerate(subMeshes):
-        tris = mesh.tris
+    for sub_idx, meshdata in enumerate(subMeshes):
+        logger.debug(f'  submesh {sub_idx:02X}')
         sub_idx = sub_idx if n_submeshes > 1 else -1
-        mesh_obj = createMesh(mesh.verts, tris, idx, sub_idx, mesh.tri2tex, mesh.mtxindex, tex_configs)
+        mesh_obj = createMesh(meshdata, tex_configs, idx, sub_idx)
         mesh_obj.parent = parent_obj
-        # logger.debug(f'  n {len(tris)} nvtx {len(mesh.verts)}')
+        logger.debug(f'  nt {len(meshdata.tris)} nv {len(meshdata.verts)}')
 
 def createModelMeshes(model, name, sc, model_obj):
     tex_configs = {}
@@ -387,7 +429,6 @@ def createModelMeshes(model, name, sc, model_obj):
         tex_configs[texnum] = tc
         # print(f'tex {texnum:04X} w {width:02X} h {height:02X}')
 
-    logger.debug('createModelMeshes')
     idx = 0
     for ro in model.rodatas:
         nodetype = ro['_node_type_']
@@ -407,59 +448,91 @@ def createModelMeshes(model, name, sc, model_obj):
         if typeDL1 or typeDL2:
         # if ro['_node_type_'] == 4 and idx == 3:
 #            print(ro)
-            print(f'createModelMesh t {nodetype:02X}')
+            print(f'createModelMesh {idx:02X} t {nodetype:02X}')
             createModelMesh(idx, model, rodata, sc, tex_configs, model_obj)
         idx += 1
-
-def clearLog():
-    f = open('D:/Mega/PD/pd_blend/pd_guns.log', 'r+')
-    f.truncate(0)
 
 def _main():
     # pdn.unregister()
     pdn.register()
 
+class PDModelPropertyGroup(PropertyGroup):
+    name: StringProperty(name='name', default='', options={'LIBRARY_EDITABLE'})
+    idx: IntProperty(name='idx', default=0, options={'LIBRARY_EDITABLE'})
+    mtx: IntProperty(name='mtx', default=0, options={'LIBRARY_EDITABLE'})
+    sub_idx: IntProperty(name='sub_idx', default=0, options={'LIBRARY_EDITABLE'})
+
+class OBJECT_PT_custom_panel(Panel):
+    bl_label = 'PD Model'
+    bl_idname = 'OBJECT_PT_custom_panel'
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "object"
+
+    @classmethod
+    def poll(cls, context):
+        return context.object and context.object.type == 'MESH'
+
+    def draw(self, context):
+        layout = self.layout
+        obj = context.object
+
+        box = layout.box()
+        box.prop(obj.pdmodel_props, 'name', icon='LOCKED', text='')
+        box.label(text=f'Index: {obj.pdmodel_props.idx:02X}', icon='LOCKED')
+        box.label(text=f'Sub Index: {obj.pdmodel_props.sub_idx:02X}', icon='LOCKED')
+        box.label(text=f'Matrix: {obj.pdmodel_props.mtx:02X}', icon='LOCKED')
+        box.enabled = False
+
+def unreg():
+    bpy.utils.unregister_class(PDModelPropertyGroup)
+    bpy.utils.unregister_class(OBJECT_PT_custom_panel)
+    del Object.pdmodel_props
+
+def clearLog():
+    f = open('D:/Mega/PD/pd_blend/pd_guns.log', 'r+')
+    f.seek(0)
+    f.truncate(0)
+
 def main():
     # pdn.unregister()
+    # unreg()
+
+    clearLog()
+
+    bpy.utils.register_class(PDModelPropertyGroup)
+    bpy.utils.register_class(OBJECT_PT_custom_panel)
+    Object.pdmodel_props = PointerProperty(type=PDModelPropertyGroup)
     pdn.register()
 
-    clearLog() # TMP
-    # clearScene()
-
+    logger.debug('clearScene()')
     pdu.clear_scene()
 
-    # createCollectionIfNone('Meshes')
-    # createCollectionIfNone('Joints')
-
     model_name = 'Gfalcon2Z'
-    # model_name = 'Gk7avengerZ'
-    # model_name = 'GdysuperdragonZ'
+    model_name = 'Gk7avengerZ'
+    model_name = 'GdysuperdragonZ'
     # model_name = 'GdydragonZ'
-    # model_name = 'Gleegun1Z'
+    model_name = 'Gleegun1Z'
     # model_name = 'GcrossbowZ'
     # model_name = 'Gdy357Z'
     # model_name = 'GdydevastatorZ'
     # model_name = 'GdyrocketZ'
     # model_name = 'GsniperrifleZ'
-    model_name = 'Gm16Z'
+    # model_name = 'Gm16Z'
     model_name = 'GshotgunZ'
     # model_name = 'GpcgunZ'
     # model_name = 'GdruggunZ'
     # model_name = 'GmaianpistolZ'
     # model_name = 'GskminigunZ'
     # model_name = 'Gz2020Z'
-    # model_name = 'Gcmp150Z'
+    model_name = 'Gcmp150Z'
     # model_name = 'PchrdragonZ'
     # model_name = 'Pchrdy357Z'
     model = readModel(model_name)
 
     model_obj = pdu.new_empty_obj(model_name)
-    meshes_obj = pdu.new_empty_obj('Meshes', model_obj)
 
     sc = 0.01
     sc = 1
-    # traverse(model, model_obj, create_joint)
     # model.traverse(create_joint, root_obj=model_obj)
-    bpy.context.view_layer.update()
-    createModelMeshes(model, model_name, sc, meshes_obj)
-
+    createModelMeshes(model, model_name, sc, model_obj)
