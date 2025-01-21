242a1febdf205c31f42186920a50e962c1996c02
Raf <>
2025-01-21 21:51:45 +0800

    Blend props for setup objects: door/lift/tinted glass/interlink objects; function to update object scale/pad bbox
    + a few utility matrix functions

--- pd_blendtools/pd_blendprops.py
+++ pd_blendtools/pd_blendprops.py
@@ -1,13 +1,20 @@
 import bpy
 from bpy.types import PropertyGroup, Object, Panel, Scene
-from bpy.props import StringProperty, IntProperty, PointerProperty, FloatVectorProperty, BoolVectorProperty
+from bpy.props import (
+    StringProperty, IntProperty, PointerProperty, FloatVectorProperty,
+    BoolVectorProperty, FloatProperty, CollectionProperty
+)
 from bpy.utils import register_class, unregister_class
-from mathutils import Vector, Matrix
+from mathutils import Euler, Vector, Matrix
 
 import nodes.nodeutils as ndu
 import pd_utils as pdu
 import tiles_import as tiles
+import setup_import as stpi
 from decl_bgtiles import *
+import pd_padsfile as pdp
+import pd_mtx as mtx
+
 
 PD_OBJTYPE_MODEL        = 0x0100
 #### BG
@@ -19,6 +26,11 @@ PD_OBJTYPE_TILE         = 0x0600
 #### Setup
 PD_OBJTYPE_PROP         = 0x0700
 PD_OBJTYPE_INTRO        = 0x0800
+
+PD_PROP_DOOR            = PD_OBJTYPE_PROP | 0x01
+PD_PROP_TINTED_GLASS    = PD_OBJTYPE_PROP | 0x2f
+PD_PROP_LIFT            = PD_OBJTYPE_PROP | 0x30
+PD_PROP_LIFT_STOP       = PD_OBJTYPE_PROP | 0xf0
 
 
 class PDObject(PropertyGroup):
@@ -124,12 +136,210 @@ class PDObject_Tile(PropertyGroup):
     roomnum: IntProperty(name='roomnum', default=0, options={'LIBRARY_EDITABLE'})
     room: PointerProperty(name='room', update=update_scene_tiles, type=bpy.types.Object, poll=check_isroom, options={'LIBRARY_EDITABLE'})
 
+# ---------------- SETUP OBJECTS ----------------
+def update_pad_bbox(self, _context):
+    obj = bpy.context.active_object
+    if not obj: return
+
+    proptype = obj.pd_obj.type
+
+    if proptype == PD_PROP_DOOR:
+        padbbox = pdp.Bbox(*self.pad_bbox)
+        bbox = pdp.Bbox(*self.model_bbox)
+        sx = (padbbox.ymax - padbbox.ymin) / (bbox.xmax - bbox.xmin)
+        sy = (padbbox.zmax - padbbox.zmin) / (bbox.ymax - bbox.ymin)
+        sz = (padbbox.xmax - padbbox.xmin) / (bbox.zmax - bbox.zmin)
+
+        if sx <= 0.000001 or sy <= 0.000001 or sz <= 0.000001:
+            sx = sy = sz = 1
+    else:
+        pd_prop = obj.pd_prop
+        pad_bbox = pdp.Bbox(*self.pad_bbox)
+        model_bbox = pdp.Bbox(*self.model_bbox)
+        modelscale = pd_prop.modelscale * pd_prop.extrascale / (256 * 4096)
+        sx, sy, sz = stpi.obj_getscale(modelscale, pad_bbox, model_bbox, pd_prop.flags)
+
+    bbox_p = pdp.Bbox(*self.pad_bbox_p)
+    bbox = pdp.Bbox(*self.pad_bbox)
+    R = mtx.rot_doorinv() if proptype == PD_PROP_DOOR else Matrix()
+    center = obj.location
+
+    normal, up, look = mtx.mtx_basis(obj.matrix_world @ R)
+    newpos = pdp.pad_pos(center, bbox_p, look, up, normal)
+    pad = pdp.Pad(newpos, look, up, normal, bbox, None)
+    center = pdp.pad_center(pad)
+    normal, up, look = mtx.mtx_basis(obj.matrix_world)
+    stpi.obj_setup_mtx(obj, look, up, center)
+
+    self.pad_pos = newpos
+    for i, val in enumerate(self.pad_bbox):
+        self.pad_bbox_p[i] = val
+
+    obj.scale = (sx, sy, sz)
+
+
+class PDObject_SetupBaseObject(PropertyGroup):
+    extrascale: IntProperty(name='extrascale', default=0, options={'LIBRARY_EDITABLE'})
+    health: IntProperty(name='health', default=0, options={'LIBRARY_EDITABLE'})
+    flags: IntProperty(name='flags', default=0, options={'LIBRARY_EDITABLE'})
+    padnum: IntProperty(name='padnum', default=0, options={'LIBRARY_EDITABLE'})
+    pad_pos: FloatVectorProperty(name='pad_pos', default=(0,0,0), size=3, options={'LIBRARY_EDITABLE'})
+    # we need to save the "previous" (before it was changed) bbox, in order to derive
+    # the original position/center of the new one
+    pad_bbox: FloatVectorProperty(name='pad_bbox', default=(0,0,0,0,0,0), size=6, update=update_pad_bbox, options={'LIBRARY_EDITABLE'})
+    pad_bbox_p: FloatVectorProperty(name='pad_bbox_p', default=(0,0,0,0,0,0), size=6, options={'LIBRARY_EDITABLE'})
+    model_bbox: FloatVectorProperty(name='pad_bbox', default=(0,0,0,0,0,0), size=6, options={'LIBRARY_EDITABLE'})
+    modelscale: FloatProperty(name='modelscale', min=0, default=0, options={'LIBRARY_EDITABLE'})
+
+
+def check_isdoor(_scene, obj):
+    return obj.pd_obj.type == PD_PROP_DOOR
+
+
+DOOR_FLAGS = [
+    ('DOORFLAG_0001',       0x0001),
+    ('Windowed',            0x0002),
+    ('DOORFLAG_0004',       0x0004),
+    ('Flip',                0x0008),
+    ('Automatic',           0x0010),
+    ('DOORFLAG_0020',       0x0020),
+    ('Rotated Pad',         0x0040),
+    ('DOORFLAG_0080',       0x0080),
+    ('DOORFLAG_0100',       0x0100),
+    ('Long Range',          0x0200),
+    ('Damage On Contact',   0x0400),
+    ('Unblockable Open',    0x0800),
+    ('DOORFLAG_4000',       0x4000),
+]
+
+DOOR_KEYFLAGS = [
+    ('10000000', 0x80),
+    ('01000000', 0x40),
+    ('00100000', 0x20),
+    ('00010000', 0x10),
+    ('00001000', 0x08),
+    ('00000100', 0x04),
+    ('00000010', 0x02),
+    ('00000001', 0x01),
+]
+
+DOORTYPES = [
+    ('Sliding',     'Sliding', 0x0),
+    ('Flexi1',      'Bunker Flexi Door (GE Only)', 0x1),
+    ('Flexi2',      'Bunker Flexi Door (GE Only)', 0x2),
+    ('Flexi3',      'Bunker Flexi Door (GE Only)', 0x3),
+    ('Vertical',    'Vertical', 0x4),
+    ('Swinging',    'Swinging', 0x5),
+    ('Eye',         'Caverns (GE Only)', 0x6),
+    ('Iris',        'Caverns (GE Only)', 0x7),
+    ('Fallaway',    'Surface Grate And Train Floor Panel (GE Only)', 0x8),
+    ('Aztec Chair', 'Aztec Door Effect', 0x9),
+    ('Hull',        'Attack Ship Windows', 0xa),
+    ('Laser',       'Lase Beam/Barricade', 0xb),
+]
+
+DOOR_SOUNDTYPES = [
+    ('00 None',                'None',                 0x00),
+    ('01 Electronic',          'Electronic',           0x01),
+    ('02 Target SFX',          'Target SFX',           0x02),
+    ('03 Hydraulic 1',         'Hydraulic 1',          0x03),
+    ('04 Skedar Ruins',        'Skedar Ruins',         0x04),
+    ('05 Skedar Elevator',     'Skedar Elevator',      0x05),
+    ('06 Wooden',              'Wooden',               0x06),
+    ('07 Target SFX',          'Target SFX',           0x07),
+    ('08 Sliding 1',           'Sliding 1',            0x08),
+    ('09 Wooden (Target SFX)', 'Wooden (Target SFX)',  0x09),
+    ('0A Roller',              'Roller',               0x0a),
+    ('0B Metal',               'Metal',                0x0b),
+    ('0C Target SFX/Latch',    'Target SFX/Latch',     0x0c),
+    ('0D Locker',              'Locker',               0x0d),
+    ('0E Target SFX',          'Target SFX',           0x0e),
+    ('0F Automatic 1',         'Automatic 1',          0x0f),
+    ('10 Stone',               'Stone',                0x10),
+    ('11 Automatic 2',         'Automatic 2',          0x11),
+    ('12 Elevator',            'Elevator',             0x12),
+    ('13 Lift 1',              'Lift 1',               0x13),
+    ('14 Lift 2',              'Lift 2',               0x14),
+    ('15 Lift 3',              'Lift 3',               0x15),
+    ('16 Lift 4',              'Lift 4',               0x16),
+    ('17 Hydraulic 2',         'Hydraulic 2',          0x17),
+    ('18 Blast Door',          'Blast Door',           0x18),
+    ('19 Automatic 1',         'Automatic 1',          0x19),
+    ('1A Lift 5',              'Lift 5',               0x1a),
+    ('1B Lift 6',              'Lift 6',               0x1b),
+    ('1C Sliding 2',           'Sliding 2',            0x1c),
+    ('1D Skedar Ship',         'Skedar Ship',          0x1d),
+    ('1E Blast Door (Alt)',    'Blast Door (Alt)',     0x1e),
+    ('1F Lift 7',              'Lift 7',               0x1f),
+    ('20 Lift 8',              'Lift 8',               0x20),
+    ('21 None',                'None',                 0x21),
+]
+
+
+class PDObject_SetupDoor(PropertyGroup):
+    def update_doorflags(self, context):
+        src = repr(self)
+
+    door_type: ndu.make_prop('door_type', {'door_type': DOORTYPES}, 'sliding', None)
+    sound_type: ndu.make_prop('sound_type', {'sound_type': DOOR_SOUNDTYPES}, '00none', None)
+
+    maxfrac: FloatProperty(name='maxfrac', min=0, default=0, options={'LIBRARY_EDITABLE'})
+    perimfrac: FloatProperty(name='perimfrac', min=0, default=0, options={'LIBRARY_EDITABLE'})
+    accel: FloatProperty(name='accel', min=0, default=0, options={'LIBRARY_EDITABLE'})
+    decel: FloatProperty(name='decel', min=0, default=0, options={'LIBRARY_EDITABLE'})
+    maxspeed: FloatProperty(name='maxspeed', min=0, default=0, options={'LIBRARY_EDITABLE'})
+    autoclosetime: FloatProperty(name='autoclosetime', min=0, default=0, description='Time To Open (In ms)', options={'LIBRARY_EDITABLE'})
+
+    door_flags: BoolVectorProperty(name='door_flags', size=len(DOOR_FLAGS), options={'LIBRARY_EDITABLE'})
+    key_flags: BoolVectorProperty(name='key_flags', size=len(DOOR_KEYFLAGS), options={'LIBRARY_EDITABLE'})
+
+    sibling: PointerProperty(name='sibling', type=bpy.types.Object, poll=check_isdoor, options={'LIBRARY_EDITABLE'})
+    laserfade: IntProperty(name='laserfade', min=0, max=255, default=0, description='Laser Opacity', options={'LIBRARY_EDITABLE'})
+
+
+class PDObject_SetupTintedGlass(PropertyGroup):
+    opadist: FloatProperty(name='opadist', default=0, options={'LIBRARY_EDITABLE'})
+    xludist: FloatProperty(name='xludist', default=0, options={'LIBRARY_EDITABLE'})
+
+
+class PDObject_SetupInterlinkObject(PropertyGroup):
+    name: StringProperty(name='name', options={'LIBRARY_EDITABLE'})
+    controller: PointerProperty(name='controller', type=bpy.types.Object, options={'LIBRARY_EDITABLE'})
+    controlled: PointerProperty(name='controlled', type=bpy.types.Object, options={'LIBRARY_EDITABLE'})
+    stopnum: IntProperty(name='lift_stop', default=0, min=1, max=4, options={'LIBRARY_EDITABLE'})
+
+
+def check_is_elevstop(_scene, obj):
+    return obj and obj.pd_obj.type == PD_PROP_LIFT_STOP
+
+
+class PDObject_SetupLift(PropertyGroup):
+    sound: IntProperty(name='sound', default=0x16, min=0, max=0x3f, options={'LIBRARY_EDITABLE'})
+    door1: PointerProperty(name='door1', type=Object, poll=check_isdoor, options={'LIBRARY_EDITABLE'})
+    door2: PointerProperty(name='door2', type=Object, poll=check_isdoor, options={'LIBRARY_EDITABLE'})
+    door3: PointerProperty(name='door3', type=Object, poll=check_isdoor, options={'LIBRARY_EDITABLE'})
+    door4: PointerProperty(name='door4', type=Object, poll=check_isdoor, options={'LIBRARY_EDITABLE'})
+    stop1: PointerProperty(name='stop1', type=Object, poll=check_is_elevstop, options={'LIBRARY_EDITABLE'})
+    stop2: PointerProperty(name='stop2', type=Object, poll=check_is_elevstop, options={'LIBRARY_EDITABLE'})
+    stop3: PointerProperty(name='stop3', type=Object, poll=check_is_elevstop, options={'LIBRARY_EDITABLE'})
+    stop4: PointerProperty(name='stop4', type=Object, poll=check_is_elevstop, options={'LIBRARY_EDITABLE'})
+    accel: FloatProperty(name='accel', default=0, options={'LIBRARY_EDITABLE'})
+    maxspeed: FloatProperty(name='maxspeed', default=0, options={'LIBRARY_EDITABLE'})
+    interlinks: CollectionProperty(name='interlinks', type=PDObject_SetupInterlinkObject)
+    active_interlink_idx: IntProperty(name='active_interlink_idx', default=0, options={'LIBRARY_EDITABLE'})
+
+
 classes = [
     PDObject,
     PDObject_Model,
     PDObject_RoomBlock,
     PDObject_Portal,
     PDObject_Tile,
+    PDObject_SetupBaseObject,
+    PDObject_SetupDoor,
+    PDObject_SetupTintedGlass,
+    PDObject_SetupInterlinkObject,
+    PDObject_SetupLift,
 ]
 
 PD_COLLECTIONS = [
@@ -174,6 +384,12 @@ def register():
     Object.pd_portal = bpy.props.PointerProperty(type=PDObject_Portal)
     Object.pd_tile = bpy.props.PointerProperty(type=PDObject_Tile)
 
+    # setup props
+    Object.pd_prop = bpy.props.PointerProperty(type=PDObject_SetupBaseObject)
+    Object.pd_door = bpy.props.PointerProperty(type=PDObject_SetupDoor)
+    Object.pd_tintedglass = bpy.props.PointerProperty(type=PDObject_SetupTintedGlass)
+    Object.pd_lift = bpy.props.PointerProperty(type=PDObject_SetupLift)
+
     n_coll = len(PD_COLLECTIONS)
     Scene.collections_vis = BoolVectorProperty(name='collections_vis', size=n_coll, default=[1]*n_coll, update=update_scene_vis, options={'LIBRARY_EDITABLE'})
     Scene.collections_sel = BoolVectorProperty(name='collections_sel', size=n_coll, default=[1]*n_coll, update=update_scene_sel, options={'LIBRARY_EDITABLE'})
@@ -188,6 +404,10 @@ def unregister():
     del Object.pd_room
     del Object.pd_model
     del Object.pd_obj
+    del Object.pd_prop
+    del Object.pd_door
+    del Object.pd_tintedglass
+    del Object.pd_lift
 
     del Scene.pd_room_goto
     del Scene.pd_tile_hilightmode
---
+++ pd_blendtools/pd_mtx.py
@@ -0,0 +1,30 @@
+from math import pi
+
+from mathutils import Euler, Vector, Matrix
+
+
+def rotM(e):
+    return Euler(e).to_matrix().to_4x4()
+
+def rot(a, axis):
+    e = (a, 0, 0)
+    if axis == 'y':
+        e = (0, a, 0)
+    elif axis == 'z':
+        e = (0, 0, a)
+
+    return rotM(e)
+
+# all doors have a rotation of 90d on the x and z axes
+# this function returns an inverse of this rotation
+def rot_doorinv():
+    Rx = rot(-pi/2,  'x')
+    Rz = rot(-pi/2,  'z')
+    return Rx @ Rz
+
+def mtx_basis(M):
+    vec = lambda mat, i: Vector([mat[k][i] for k in range(3)])
+
+    _loc, rotmat, _sca = M.decompose()
+    R = rotmat.to_matrix()
+    return vec(R, 0), vec(R, 1), vec(R, 2)
